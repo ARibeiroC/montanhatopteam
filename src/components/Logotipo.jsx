6d06e095d768e010819fcc51cfb462e2f36bd2b0
ARibeiroC <andersonspl@hotmail.com>
2024-09-04 05:51:40 -0300

    fix: erro de imagem no projeto de desenvolvimento.

--- src/components/Logotipo.jsx
+++ src/components/Logotipo.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Logotipo.css'
+import logoImage from '../img/logo-mountain-shadow-white.png'
+import textImage from '../img/text-montanha-white.png'
 
 function Logotipo() {
   return (
     <div className="logo">
-        <img src="/logo--mountain-shadow-white.png" alt="Logo MONTANHA TOP TEAM" id='img-logo'/>
-        <img src="text-montanha-white.png" alt="texto Montanha da logo" id='text-logo' />
+        <img src={logoImage} alt="Logo MONTANHA TOP TEAM" id='img-logo'/>
+        <img src={textImage} alt="texto Montanha da logo" id='text-logo' />
     </div>
   )
 }
